37b47cefceb7b4991fbf2557ded7e406d45fcaae
Nicholas Muldrew <nick.muldrew@EFTSPEC.LOCAL>
2019-10-29 17:05:51 +1300

    Added files

---
+++ PhotoMosaic/Image.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoMosaic {
+    class Image {
+        private string ImageURI;
+        public Bitmap bitmap;
+        private Graphics Canvas;
+        private Color AvgColor;
+        private int AvgBrightness;
+        public int Width;
+        public int Height;
+        public readonly static int Cells = 16;
+
+        public Image(Graphics Canvas)
+        {
+            Canvas = this.Canvas;
+            ImageURI = @"C:\Users\nick.muldrew\Downloads\4_Squares.png";
+            bitmap = new Bitmap(ImageURI);
+            Canvas.DrawImage(bitmap, 800, 800);
+        }
+
+        public Color CalculateAverageColor()
+        {
+            Color CalculateAverageColor = new Color();
+
+            return CalculateAverageColor;
+        }
+
+        public int CalculateAverageBrightness()
+        {
+            int CalculatedBrightness = 0;
+
+            return CalculatedBrightness;
+        }
+    }
+}
---
+++ PhotoMosaic/SourceImage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoMosaic {
+    class SourceImage : Image{
+        Color[,] AVGColors = new Color[Cells, Cells];         //Color array with average colours.
+        
+        public SourceImage(Graphics Canvas) : base(Canvas)
+        {
+
+        }
+
+        public void CalculateAVGCellColors()
+        {
+            for(int w = 0; w < Cells; w++)
+            {
+                for(int h = 0; h < Cells; h++)
+                {
+
+                }
+            }
+        }
+
+        private void CalculateSection(int x, int y)
+        {
+            //Tuple<int, int, int> tuple = new Tuple<int, int, int>(0, 0, 0);
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            for(int w = x; w < x + Cells; w++)
+            {
+                for(int h = y; h < y + Cells; h++)
+                {
+                    r += 0;
+                }
+            }
+        }
+    }
+}
